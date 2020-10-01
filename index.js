1b613b9d64b9576f652edde8fb6eb4c47efa3972
Immanuel Ngari <imanga254@gmail.com>
2020-10-01 15:27:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(){
+  return `${5+2}`
+}
+function subtract() {
+  return `${5-3}`
+}
+function multiply() {
+  return `${5*3}`
+}
+function divide() {
+  return `${5/3}`
+}
+function add(a,b) {
+  var result = a + b
+  return result;
+}
+ add(700 + 99)
+
+ function subtract(a,b) {
+   var result = a - b
+   return result
+}
+subtract(700 - 99)
+
+function multiply(a,b) {
+  var result = a * b
+  return result
+}
+multiply(5*3)
+
+function divide(a,b) {
+  var result = a/b
+  return result
+}
+divide(5/3)
+
+function increment(n) {
+  var result = ++n
+  return result
+}
+increment(5)
+
+function decrement(n) {
+  var result = --n
+  return result
+}
+decrement(6)
+
+makeInt('20',10)
